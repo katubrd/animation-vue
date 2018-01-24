3e83e02814417952f4e74e16d02542051de56345
Katu_brd <katuthings@gmail.com>
2018-01-24 12:06:04 +0100

    basics

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'My first vue file'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'heeeey u!'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
